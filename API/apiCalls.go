54249e12c8c9b1aa5ab3a363f781fd822208444a
Daniel Dos Santos <dos@Daniels-MBP.attlocal.net>
2024-05-22 18:21:39 -0400

    added sports map

--- API/apiCalls.go
+++ API/apiCalls.go
@@ -168,3 +168,53 @@ func GetAllUpcomingEvents() []Event {
   return allEvents
 }
 
+
+func GetAllUpcomingEventsMap() map[string][]Event {
+  var apiKey string = getAPIKEY()
+  var eventMap = make(map[string][]Event)
+
+  for _, sport := range sport.AllSports() {
+
+    req, err := http.Get(fmt.Sprintf("https://api.the-odds-api.com/v4/sports/%s/odds/?apiKey=%s&regions=us&markets=h2h,spreads,totals&oddsFormat=american", sport, apiKey))      
+
+    if err != nil {
+      fmt.Print(err.Error())
+    }
+
+    defer req.Body.Close()
+    response, err := io.ReadAll(req.Body)
+
+    if err != nil {
+      fmt.Print(err.Error())
+    }
+
+    var events []Event
+    msg := json.Unmarshal(response, &events)
+
+    if msg  != nil {
+      fmt.Println(err)
+    }
+
+
+    for i, event := range events {
+      // Initialize a slice to store filtered bookmakers
+      var filteredBookmakers []Bookmaker
+      // Iterate over each bookmaker
+      for _, bookmaker := range event.Bookmakers {
+        if bookmaker.Title == "FanDuel" {
+          // If it is, append it to the filteredBookmakers slice
+          filteredBookmakers = append(filteredBookmakers, bookmaker)
+        }
+      }
+
+      if len(filteredBookmakers) == 0 && len(event.Bookmakers) != 0 {
+        filteredBookmakers = append(filteredBookmakers, event.Bookmakers[0])
+      }
+      events[i].Bookmakers = filteredBookmakers
+    }
+    eventMap[string(sport)] = events
+  }
+
+  return eventMap
+}
+
--- main.go
+++ main.go
@@ -1,41 +1,73 @@
 package main
 import(
-	"oddshub/API"
-	"fmt"
+  "oddshub/API"
+  "fmt"
 )
 
-	func main() {
-     
-	  //API.GetAllUpcomingEvents()
-	  printInfo(API.GetAllUpcomingEvents())
-	}
+func main() {
+  printInfoMap(API.GetAllUpcomingEventsMap())
+  //API.GetAllUpcomingEvents()
+  //printInfo(API.GetAllUpcomingEvents())
+}
 
 func printInfo(events []API.Event) {
 
-	  for _, event := range events {
+  for _, event := range events {
 
-	    fmt.Printf("ID: %s\n", event.ID)
-	    fmt.Printf("Sport: %s\n", event.SportTitle)
-	    fmt.Printf("Commence Time: %s\n", event.CommenceTime)
-	    fmt.Printf("Home Team: %s\n", event.HomeTeam)
-	    fmt.Printf("Away Team: %s\n", event.AwayTeam)
-	    fmt.Println("Bookmakers:")
+    fmt.Printf("ID: %s\n", event.ID)
+    fmt.Printf("Sport: %s\n", event.SportTitle)
+    fmt.Printf("Commence Time: %s\n", event.CommenceTime)
+    fmt.Printf("Home Team: %s\n", event.HomeTeam)
+    fmt.Printf("Away Team: %s\n", event.AwayTeam)
+    fmt.Println("Bookmakers:")
 
 
-	    for _, bookmaker := range event.Bookmakers {
-	      fmt.Printf("  - Title: %s\n", bookmaker.Title)
-	      fmt.Println("    Markets:")
-	      for _, market := range bookmaker.Markets {
-	        fmt.Printf("      - Key: %s\n", market.Key)
-	        fmt.Printf("        Last Update: %s\n", market.LastUpdate)
-	        fmt.Println("        Outcomes:")
-	        for _, outcome := range market.Outcomes {
-	          fmt.Printf("          - Name: %s, Price: %f, Point: %f\n", outcome.Name, outcome.Price, outcome.Point)
-	        }
-	      }
-	    }
+    for _, bookmaker := range event.Bookmakers {
+      fmt.Printf("  - Title: %s\n", bookmaker.Title)
+      fmt.Println("    Markets:")
+      for _, market := range bookmaker.Markets {
+        fmt.Printf("      - Key: %s\n", market.Key)
+        fmt.Printf("        Last Update: %s\n", market.LastUpdate)
+        fmt.Println("        Outcomes:")
+        for _, outcome := range market.Outcomes {
+          fmt.Printf("          - Name: %s, Price: %f, Point: %f\n", outcome.Name, outcome.Price, outcome.Point)
+        }
+      }
+    }
 
-	    fmt.Println()
-	  }
-	}
+    fmt.Println()
+  }
+}
 
+
+func printInfoMap(eventsMap map[string][]API.Event) {
+
+  for sportName, events := range eventsMap {
+    fmt.Println(sportName)
+    for _, event := range events {
+
+      fmt.Printf("ID: %s\n", event.ID)
+      fmt.Printf("Sport: %s\n", event.SportTitle)
+      fmt.Printf("Commence Time: %s\n", event.CommenceTime)
+      fmt.Printf("Home Team: %s\n", event.HomeTeam)
+      fmt.Printf("Away Team: %s\n", event.AwayTeam)
+      fmt.Println("Bookmakers:")
+
+
+      for _, bookmaker := range event.Bookmakers {
+        fmt.Printf("  - Title: %s\n", bookmaker.Title)
+        fmt.Println("    Markets:")
+        for _, market := range bookmaker.Markets {
+          fmt.Printf("      - Key: %s\n", market.Key)
+          fmt.Printf("        Last Update: %s\n", market.LastUpdate)
+          fmt.Println("        Outcomes:")
+          for _, outcome := range market.Outcomes {
+            fmt.Printf("          - Name: %s, Price: %f, Point: %f\n", outcome.Name, outcome.Price, outcome.Point)
+          }
+        }
+      }
+
+      fmt.Println()
+    }
+  }
+}
