ef064385ef0c588e26518c61c94aaf785fc3d354
Daniel Dos Santos <notdos22@gmail.com>
2024-06-27 13:39:34 -0400

    Adding Brazil Campeonato colors

--- colors/campeonato_soccer_colors.go
+++ colors/campeonato_soccer_colors.go
@@ -1,30 +1,35 @@
 /*
- * Copyright (c) 2024 dos-2
- * All rights reserved.
- */
+* Copyright (c) 2024 dos-2
+* All rights reserved.
+*/
 package colors
 
 import "github.com/dos-2/oddshub/models"
 
 var CampeonatoTeamColorMap = map[string]models.TeamColors{
-	"América Mineiro":      {PrimaryColor: "#2E8B57", SecondaryColor: "#FFFFFF"},
-	"Athletico Paranaense": {PrimaryColor: "#E60000", SecondaryColor: "#000000"},
-	"Atlético Goianiense":  {PrimaryColor: "#000000", SecondaryColor: "#FF0000"},
-	"Atlético Mineiro":     {PrimaryColor: "#000000", SecondaryColor: "#FFFFFF"},
-	"Bahia":                {PrimaryColor: "#0033A0", SecondaryColor: "#FF0000"},
-	"Botafogo":             {PrimaryColor: "#000000", SecondaryColor: "#FFFFFF"},
-	"Corinthians":          {PrimaryColor: "#000000", SecondaryColor: "#FFFFFF"},
-	"Coritiba":             {PrimaryColor: "#006600", SecondaryColor: "#FFFFFF"},
-	"Cruzeiro":             {PrimaryColor: "#0033A0", SecondaryColor: "#FFFFFF"},
-	"Cuiabá":               {PrimaryColor: "#008000", SecondaryColor: "#FFFF00"},
-	"Flamengo":             {PrimaryColor: "#E60000", SecondaryColor: "#000000"},
-	"Fluminense":           {PrimaryColor: "#830000", SecondaryColor: "#006633"},
-	"Fortaleza":            {PrimaryColor: "#0000FF", SecondaryColor: "#FF0000"},
-	"Goiás":                {PrimaryColor: "#008000", SecondaryColor: "#FFFFFF"},
-	"Grêmio":               {PrimaryColor: "#0000FF", SecondaryColor: "#000000"},
-	"Internacional":        {PrimaryColor: "#FF0000", SecondaryColor: "#FFFFFF"},
-	"Palmeiras":            {PrimaryColor: "#006633", SecondaryColor: "#FFFFFF"},
-	"Red Bull Bragantino":  {PrimaryColor: "#FFFFFF", SecondaryColor: "#FF0000"},
-	"Santos":               {PrimaryColor: "#000000", SecondaryColor: "#FFFFFF"},
-	"São Paulo":            {PrimaryColor: "#FF0000", SecondaryColor: "#000000"},
+  "América Mineiro":      {PrimaryColor: "#000000", SecondaryColor: "#FFFFFF"},
+  "Athletico Paranaense": {PrimaryColor: "#E60000", SecondaryColor: "#000000"},
+  "Atlético Goianiense":  {PrimaryColor: "#000000", SecondaryColor: "#FF0000"},
+  "Atlético Mineiro":     {PrimaryColor: "#000000", SecondaryColor: "#FFFFFF"},
+  "Bahia":                {PrimaryColor: "#0033A0", SecondaryColor: "#FF0000"},
+  "Botafogo":             {PrimaryColor: "#000000", SecondaryColor: "#FFFFFF"},
+  "Bragantino-SP": {PrimaryColor: "#ED1C24", SecondaryColor: "#000000"},
+  "Corinthians":          {PrimaryColor: "#000000", SecondaryColor: "#FFFFFF"},
+  "Coritiba":             {PrimaryColor: "#006600", SecondaryColor: "#FFFFFF"},
+  "Cruzeiro":             {PrimaryColor: "#0033A0", SecondaryColor: "#FFFFFF"},
+  "Cuiabá":               {PrimaryColor: "#008000", SecondaryColor: "#FFFF00"},
+  "Criciuma": {PrimaryColor: "#003399", SecondaryColor: "#FFFFFF"},
+  "Flamengo":             {PrimaryColor: "#E60000", SecondaryColor: "#000000"},
+  "Fluminense":           {PrimaryColor: "#830000", SecondaryColor: "#006633"},
+  "Fortaleza":            {PrimaryColor: "#0000FF", SecondaryColor: "#FF0000"},
+  "Goiás":                {PrimaryColor: "#008000", SecondaryColor: "#FFFFFF"},
+  "Grêmio":               {PrimaryColor: "#0000FF", SecondaryColor: "#000000"},
+  "Internacional":        {PrimaryColor: "#FF0000", SecondaryColor: "#FFFFFF"},
+  "Juventude": {PrimaryColor: "#1B7742", SecondaryColor: "#FFFFFF"},
+  "Palmeiras":            {PrimaryColor: "#006633", SecondaryColor: "#FFFFFF"},
+  "Red Bull Bragantino":  {PrimaryColor: "#FFFFFF", SecondaryColor: "#FF0000"},
+  "Santos":               {PrimaryColor: "#000000", SecondaryColor: "#FFFFFF"},
+  "Sao Paulo":            {PrimaryColor: "#FF0000", SecondaryColor: "#000000"},
+  "Vasco da Gama": {PrimaryColor: "#FFFFFF", SecondaryColor: "#002D5D"},
+  "Vitoria": {PrimaryColor: "#D62D45", SecondaryColor: "#FFFFFF"},
 }
