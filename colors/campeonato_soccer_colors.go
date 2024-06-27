243d99585eea1c11cf20275b78f3f3586035b5bb
Daniel Dos Santos <notdos22@gmail.com>
2024-06-27 13:42:43 -0400

    fixed atletico goianiense color bug

--- colors/campeonato_soccer_colors.go
+++ colors/campeonato_soccer_colors.go
@@ -9,7 +9,7 @@ import "github.com/dos-2/oddshub/models"
 var CampeonatoTeamColorMap = map[string]models.TeamColors{
   "América Mineiro":      {PrimaryColor: "#000000", SecondaryColor: "#FFFFFF"},
   "Athletico Paranaense": {PrimaryColor: "#E60000", SecondaryColor: "#000000"},
-  "Atlético Goianiense":  {PrimaryColor: "#000000", SecondaryColor: "#FF0000"},
+  "Atletico Goianiense":  {PrimaryColor: "#FF0000", SecondaryColor: "#000000"},
   "Atlético Mineiro":     {PrimaryColor: "#000000", SecondaryColor: "#FFFFFF"},
   "Bahia":                {PrimaryColor: "#0033A0", SecondaryColor: "#FF0000"},
   "Botafogo":             {PrimaryColor: "#000000", SecondaryColor: "#FFFFFF"},
